f73758e56f5d73435d083e2eb66a0eb691911b88
Gavin Sharp <gavin@properly.ca>
2020-10-06 10:15:49 -0400

    chore: disable jest/no-deprecated-functions rule in lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
     extends: ["@goproperly/eslint-config-properly-base"],
+    rules: {
+      // Override base config, we don't use Jest in this project
+      'jest/no-deprecated-functions': 'off'
+    }
 };
